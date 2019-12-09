5737988b9ac582f4c224e22156eb5d9c64f442d7
Elichai Turkel <elichai.turkel@gmail.com>
2019-12-10 00:31:13 +0200

    Add tests for schnorr

--- secp256k1_test.go
+++ secp256k1_test.go
@@ -3,6 +3,8 @@ package secp256k1
 import "testing"
 import "math/rand"
 
+// TODO: Add test vectors. (even though they're included in libsecp itself)
+
 func fastGeneratePrivateKey(t *testing.T) (key *PrivateKey) {
 	buf := [32]byte{}
 	for {
@@ -17,7 +19,30 @@ func fastGeneratePrivateKey(t *testing.T) (key *PrivateKey) {
 	}
 }
 
-func TestParsePubKey(t *testing.T) {
+func TestParseSerializePrivateKey(t *testing.T) {
+	for i := 0; i < 150; i++ {
+		privkey := fastGeneratePrivateKey(t)
+
+		serialized := privkey.Serialize()
+		privkey2, err := ParsePrivateKey(serialized)
+		if err != nil {
+			t.Errorf("Failed parsing privkey %v", err)
+		}
+
+		if *privkey != *privkey2 {
+			t.Errorf("Privkeys aren't equal %v %v", privkey2, privkey)
+		}
+	}
+}
+
+func TestGeneratePrivateKey(t *testing.T) {
+	_, err := GeneratePrivateKey()
+	if err != nil {
+		t.Errorf("Failed generating a privatekey %v", err)
+	}
+}
+
+func TestParseECDSAPubKey(t *testing.T) {
 	for i := 0; i < 150; i++ {
 		privkey := fastGeneratePrivateKey(t)
 		pubkey := privkey.GenerateEcdsaPublicKey()
@@ -57,25 +82,41 @@ func TestSignVerifyECDSA(t *testing.T) {
 	}
 }
 
-func TestParseSerializePrivateKey(t *testing.T) {
+func TestParseSchnorrPubKey(t *testing.T) {
 	for i := 0; i < 150; i++ {
 		privkey := fastGeneratePrivateKey(t)
+		pubkey := privkey.GenerateSchnorrPublicKey()
+		serialized := pubkey.Serialize()
 
-		serialized := privkey.Serialize()
-		privkey2, err := ParsePrivateKey(serialized)
+		pubkeyNew, err := ParseSchnorrPubKey(serialized)
 		if err != nil {
-			t.Errorf("Failed parsing privkey %v", err)
+			t.Errorf("Failed Parsing the public key: %v", err)
 		}
 
-		if *privkey != *privkey2 {
-			t.Errorf("Privkeys aren't equal %v %v", privkey2, privkey)
+		if pubkey != *pubkeyNew {
+			t.Errorf("Pubkeys aren't the same: %v, %v", pubkey, pubkeyNew)
 		}
 	}
 }
 
-func TestGeneratePrivateKey(t *testing.T) {
-	_, err := GeneratePrivateKey()
-	if err != nil {
-		t.Errorf("Failed generating a privatekey %v", err)
+func TestSignVerifySchnorr(t *testing.T) {
+	for i := 0; i < 150; i++ {
+		privkey := fastGeneratePrivateKey(t)
+
+		pubkey := privkey.GenerateSchnorrPublicKey()
+		msg := [32]byte{}
+		n, err := rand.Read(msg[:])
+		if err != nil || n != 32 {
+			t.Errorf("Failed generating a msg %v %d", err, n)
+		}
+		sig1 := privkey.SchnorrSign(msg)
+		sig2 := privkey.SchnorrSign(msg)
+		if sig1 != sig2 {
+			t.Errorf("Signing isn't deterministic %v %v", sig1, sig2)
+		}
+		if !pubkey.SchnorrVerify(msg, sig1) {
+			t.Errorf("Failed verifying ECDSA signature privkey: %v pubkey: %v signature: %v", privkey, pubkey, sig1)
+		}
+
 	}
 }
