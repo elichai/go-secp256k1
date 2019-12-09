7ba223aa9527308b9690d90eb2b31f6de49391d5
Elichai Turkel <elichai.turkel@gmail.com>
2019-12-09 23:32:16 +0200

    Add tests for ecdsa and private key

---
+++ secp256k1_test.go
@@ -0,0 +1,81 @@
+package secp256k1
+
+import "testing"
+import "math/rand"
+
+func fastGeneratePrivateKey(t *testing.T) (key *PrivateKey) {
+	buf := [32]byte{}
+	for {
+		n, err := rand.Read(buf[:])
+		if err != nil || n != len(buf) {
+			t.Fatalf("Failed generating a privatekey %v", err)
+		}
+		privkey, err := ParsePrivateKey(buf)
+		if err == nil {
+			return privkey
+		}
+	}
+}
+
+func TestParsePubKey(t *testing.T) {
+	for i := 0; i < 150; i++ {
+		privkey := fastGeneratePrivateKey(t)
+		pubkey := privkey.GenerateEcdsaPublicKey()
+		serialized := pubkey.Serialize()
+
+		pubkeyNew, err := ParseEcdsaPubKey(serialized[:])
+		if err != nil {
+			t.Errorf("Failed Parsing the public key: %v", err)
+		}
+
+		if pubkey != *pubkeyNew {
+			t.Errorf("Pubkeys aren't the same: %v, %v", pubkey, pubkeyNew)
+		}
+
+	}
+}
+
+func TestSignVerifyECDSA(t *testing.T) {
+	for i := 0; i < 150; i++ {
+		privkey := fastGeneratePrivateKey(t)
+
+		pubkey := privkey.GenerateEcdsaPublicKey()
+		msg := [32]byte{}
+		n, err := rand.Read(msg[:])
+		if err != nil || n != 32 {
+			t.Errorf("Failed generating a msg %v %d", err, n)
+		}
+		sig1 := privkey.EcdsaSign(msg)
+		sig2 := privkey.EcdsaSign(msg)
+		if sig1 != sig2 {
+			t.Errorf("Signing isn't deterministic %v %v", sig1, sig2)
+		}
+		if !pubkey.EcdsaVerify(msg, sig1) {
+			t.Errorf("Failed verifying ECDSA signature privkey: %v pubkey: %v signature: %v", privkey, pubkey, sig1)
+		}
+
+	}
+}
+
+func TestParseSerializePrivateKey(t *testing.T) {
+	for i := 0; i < 150; i++ {
+		privkey := fastGeneratePrivateKey(t)
+
+		serialized := privkey.Serialize()
+		privkey2, err := ParsePrivateKey(serialized)
+		if err != nil {
+			t.Errorf("Failed parsing privkey %v", err)
+		}
+
+		if *privkey != *privkey2 {
+			t.Errorf("Privkeys aren't equal %v %v", privkey2, privkey)
+		}
+	}
+}
+
+func TestGeneratePrivateKey(t *testing.T) {
+	_, err := GeneratePrivateKey()
+	if err != nil {
+		t.Errorf("Failed generating a privatekey %v", err)
+	}
+}
